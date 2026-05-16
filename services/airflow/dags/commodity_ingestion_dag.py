800317f51b21ba8efda46e959b51380f9a6f5178
anas325 <91235726+anas325@users.noreply.github.com>
2026-05-16 02:13:09 +0100

    feat: Implement commodity ingestion DAG with DockerOperator

---
+++ services/airflow/dags/commodity_ingestion_dag.py
@@ -0,0 +1,42 @@
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+LOG_HOST_PATH = os.environ.get("PIPELINE_LOG_HOST_PATH")
+
+with DAG(
+    dag_id="commodity_ingestion",
+    schedule="0 18 * * 1-5",
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["commodities", "postgres"],
+) as dag:
+    mounts = []
+    if LOG_HOST_PATH:
+        mounts.append(
+            Mount(source=LOG_HOST_PATH, target="/var/log/pfa_bvc", type="bind")
+        )
+
+    DockerOperator(
+        task_id="ingest_commodities",
+        image="pfa_bvc_pipelines:latest",
+        command="uv run python -m comodities.commodity_ingestion --mode daily",
+        working_dir="/pipelines",
+        environment={
+            "PYTHONPATH": "/pipelines",
+            "PG_HOST": "postgres",
+            "PG_PORT": "5432",
+            "PG_DB": "pfa_bvc",
+            "PG_USER": "postgres",
+            "PG_PASSWORD": os.environ.get("PG_PASSWORD", "postgres"),
+            "PIPELINE_LOG_DB": "/var/log/pfa_bvc/pipeline_logs.db",
+        },
+        mounts=mounts,
+        network_mode="bvc_net",
+        auto_remove="force",
+        docker_url="unix://var/run/docker.sock",
+        mount_tmp_dir=False,
+    )
